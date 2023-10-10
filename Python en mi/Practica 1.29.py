354b3c8657de84c1201cedac0cfa2928e6010040
fbatlos123 <fbatlos123@g.educaand.es>
2023-10-10 19:10:54 +0200

    10/10

--- Python en mi/Practica 1.29.py
+++ Python en mi/Practica 1.29.py
@@ -8,10 +8,17 @@
 #escribe Te llamas nom y tienes edad años, te quedan aún vida años por cumplir"
 nom=input("Dime tu nombre: ")
 edad=int(input("Dime tu edad: "))
+
 if nom==" ":
     nom="Desconocido"
+    while edad<0 or edad>125:
+        print("ERROR")
+        edad=int(input("Dime tu edad: "))  
 else:
     while edad<0 or edad>125:
-        print("Edad no valida")
+        print("ERROR")
+        edad=int(input("Dime tu edad: "))   
+
 vida=125-edad
+
 print(f"Te llamas {nom} y tienes {edad} años, te quedan aún {vida} años por cumplir")
---
+++ Python en mi/Practica 1.30.py
@@ -0,0 +1,19 @@
+
+#lee numero,incremento,total
+#fin=0
+#mientras incremento!=0 or total!=0 
+#   lee incremeto, total 
+#mierntras numero=total 
+#   numero+=incremento
+#   escribe numero-
+#
+#
+#
+#
+numero=int(input("Dime tu número: "))
+incremento=int(input("De cuanto va ha ser el incremento: "))
+total=int(input("Dime hasta donde tiene que llegar: "))
+while numero<total+1:
+    print(numero)
+    numero+=incremento
+   
