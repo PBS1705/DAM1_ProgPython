8eb48b1eb764df43058d82347a501056d58bbeb0
fbatlos123 <fbatlos123@g.educaand.es>
2023-10-10 19:28:39 +0200

    10/10

--- Python en mi/Practica 1.30.py
+++ Python en mi/Practica 1.30.py
@@ -1,19 +1,12 @@
 
 #lee numero,incremento,total
-#fin=0
-#mientras incremento!=0 or total!=0 
-#   lee incremeto, total 
-#mierntras numero=total 
-#   numero+=incremento
-#   escribe numero-
-#
-#
-#
-#
+#fin lista desde numero hasta total
+# escribe fin
+#  
 numero=int(input("Dime tu número: "))
 incremento=int(input("De cuanto va ha ser el incremento: "))
 total=int(input("Dime hasta donde tiene que llegar: "))
-while numero<total+1:
-    print(numero)
-    numero+=incremento
+fin=list(range(numero-1,total+1))
+fin=fin[numero:total:incremento]
+print(fin)
    
--- holamun.py
+++ holamun.py
@@ -1,1 +1,6 @@
-print("Hola")
+numero=int(input("Dime tu número: "))
+incremento=int(input("De cuanto va ha ser el incremento: "))
+total=int(input("Dime hasta donde tiene que llegar: "))
+pan=list(range(numero,total))
+tot=pan[numero:total:incremento]
+print(tot)
