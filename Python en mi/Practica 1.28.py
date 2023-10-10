29bc7ae01503b39e0009b18f6ac51839556ad8fe
fbatlos123 <fbatlos123@g.educaand.es>
2023-10-10 19:36:20 +0200

    10/10

--- Python en mi/Practica 1.28.py
+++ Python en mi/Practica 1.28.py
@@ -12,18 +12,10 @@
 #fin
 n1=int(input("Dame un número: "))
 n2=int(input("Dame otro número: "))
-cont=0
-nc=n1
-nc2=n2
+cont=n1-n2
 if n1==n2:
     print("Los números no pueden ser iguales ")
 if n1>n2:
-        while n2<n1:
-            n2+=1
-            cont+=1
-        print(f"El número menor es el {nc2} y entre ellos existen {cont} números enteros")
+    print(f"El número menor es el {n2} y entre ellos existen {cont} números enteros")
 else:
-    while n1<n2:
-        n1+=1
-        cont+=1
-    print(f"El número menor es el {nc} y entre ellos existen {cont} números enteros")
+    print(f"El número menor es el {n1} y entre ellos existen {-cont} números enteros")
