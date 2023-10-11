2d7a20572e56041bfbf448cc8abd6cd507d6ceb5
fbatlos123 <fbatlos123@g.educaand.es>
2023-10-11 18:02:36 +0200

    11/10

--- UD1/P1.7/Practica 1.30.py
+++ UD1/P1.7/Practica 1.30.py
@@ -1,2 +1,20 @@
+num=int(input("Introduce el número: "))
+incremento=int(input("Introduce el incremento: "))
+total=int(input("Introduce el total: "))
 
+while incremento<=0 or total<=0:
+    print("Total e incremento deben ser mayor de 0")
+    incremento=int(input("Introduce el incremento: "))
+    total=int(input("Introduce el total: "))
+
+print("Serie=>", end=" ")
+print(num,end="-")
+while num<total-1:
+    num+=incremento
+    if num==total-1:
+        print(num+1, end="")
+    elif num!=total-1:
+        print(num+1, end="..")
+
+print(f"-{total}",end="")
    
---
+++ UD1/P1.8/P1.82.py
@@ -0,0 +1,20 @@
+#Inicio
+#
+#	Escribe "Introduce un número: "
+#	Lee x
+#	Escribe "Introduce otro: "
+#	Lee y
+#	
+#	Si (x >= y) entonces
+#		numIni = x - 1
+#		numFin = y
+#	Sino
+#		numIni = y - 1
+#		numFin = x
+#		
+#	Para i en (numIni...numFin) hacer
+#		Escribe i + 
+#		Si (numIni != numFin) entonces
+#			Escribe "-"
+#
+#Fin
