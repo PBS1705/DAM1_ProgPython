634ecfca4cab753ee140a984fe5d47834fdf890e
fbatlos123 <fbatlos123@g.educaand.es>
2023-10-17 17:38:46 +0200

    17/10

--- UD1/P1.2/#P5.py
+++ UD1/P1.2/#P5.py
@@ -1,15 +1,5 @@
 #P5
-print("Dime el importe del producto :")
-inp=float(input())
-print("Dime si es tipo 1 (21%),2(10%),3(4%)")
-tip=int(input())   
-if(tip==1):
-    print("Tu producto con iva: ",inp=+(inp*0.21))
-else:
-    if(tip==2):
-       print("Tu producto con iva: ",inp=+(inp*0.1))
-    else:
-     if(tip==3):
-        print("Tu producto con iva: ",inp=+(inp*0.04))
-     else:
-        print("ERROR")
+precio_sin_iva = float(input("Ingresa el precio: "))
+porcentaje_iva = float(input("Ingresa el porcentaje de IVA: "))
+precio_final = precio_sin_iva * (1 + porcentaje_iva / 100)
+print("El precio final es: ", precio_final, "€")
---
+++ UD1/P1.9/P1.9.5.py
@@ -0,0 +1,5 @@
+def parte5():
+    precio_sin_iva = float(input("Ingresa el precio: "))
+    porcentaje_iva = float(input("Ingresa el porcentaje de IVA: "))
+    precio_final = precio_sin_iva * (1 + porcentaje_iva / 100)
+    print("El precio final es: ", precio_final, "€")
