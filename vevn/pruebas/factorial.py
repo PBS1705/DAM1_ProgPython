fe51387d9520c4aab5b488a26fbcb3abc1ce0f8c
fbatlos123 <fbatlos123@g.educaand.es>
2023-10-17 19:07:05 +0200

    17/10

---
+++ vevn/pruebas/factorial.py
@@ -0,0 +1,8 @@
+num=int(input("Dame un numero para hacerle el factoria: "))
+while num<0:
+    num=int(input("Dame otro numero para hacerle el factoria: "))
+cont=num
+while num>1:
+    num-=1
+    cont*=num
+print(f"Su factorial es : {cont}")
