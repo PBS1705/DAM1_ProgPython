f4042209a039fc025cfa584344922637c0ce8c44
fbatlos123 <fbatlos123@g.educaand.es>
2023-10-23 19:25:41 +0200

    1

--- PyTest/pruebas/factorial.py
+++ PyTest/pruebas/factorial.py
@@ -1,9 +1,10 @@
-def factorial(num):
-    num=int(input("Dame un numero para hacerle el factoria: "))
-    while num<0:
-        num=int(input("Dame otro numero para hacerle el factoria: "))
-    cont=num
-    while num>1:
-        num-=1
-        cont*=num
-    return cont
+
+num=int(input("Dame un numero para hacerle el factoria: "))
+while num<0:
+    num=int(input("Dame otro numero para hacerle el factoria: "))
+cont=num
+inicio=f"Tu {num}! => "
+while num>1:
+    num-=1
+    cont*=num
+print(inicio,cont)
