75f050661a6623f0403f1e7b987205020320418a
fbatlos123 <fbatlos123@g.educaand.es>
2023-10-23 21:23:53 +0200

    10

--- UD1/P1.9/P1.9.11.py
+++ UD1/P1.9/P1.9.11.py
@@ -6,7 +6,8 @@ def const():
         sumar=float((n*(n+1))/2)
         n=n-1
         nom +=str(sumar)
-        nom+=str(" / ")
+        if sumar !=1:   
+           nom+=str(" / ")
     return nom
 
 print(const())
